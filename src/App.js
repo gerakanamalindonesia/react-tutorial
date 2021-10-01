6f2315de195c6dce7050b2f00077945b7946a15c
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2021-10-01 17:18:56 +0700

    install dan penggunaan react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import {useState, useEffect} from 'react'
+import {Button, Alert} from 'react-bootstrap'
 import './App.css';
 import User from "./User";
 
 function App() {
-    const [data, setData] = useState(10)
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        console.log("useEffect data with data " + data)
-    }, [data])
-
-    useEffect(() => {
-        console.log("useEffect count with count " + count)
-    }, [count])
-
     return (
         <div className="App">
+            <h1>Bootstrap</h1>
+            <div style={{marginBottom: '10px'}}>
+                <Button variant="warning" onClick={() => {alert("Mantabb, kamu sah selesai")}}>Click</Button>
+            </div>
 
-            <User count = {count} data = {data} />
-
-            <button onClick={() => setCount(count + 1)} >Update Count</button>
-            <button onClick={() => setData(data + 1)} >Update Data</button>
+            <div>
+                {
+                    [
+                        'primary',
+                        'secondary',
+                        'success',
+                        'danger',
+                        'warning',
+                        'info',
+                        'light',
+                        'dark'
+                    ].map((variant, idx) => (
+                        <Alert key={idx} variant={variant}>
+                            Ini adalah alert dengan tipe {variant}
+                        </Alert>
+                    ))
+                }
+            </div>
         </div>
     )
 }
