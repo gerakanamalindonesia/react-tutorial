dd288fe9ba77056706d62e781adcfef5a4aa00f2
gerakanamalindonesia <80880673+gerakanamalindonesia@users.noreply.github.com>
2021-10-01 14:30:46 +0700

    pengenalan useEffect hooks (#10)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import {useState, use} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
-    const [data, setData] = useState("Irwanto")
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("Irwanto")
+
+    // useEffect berjalan setelah render
+    useEffect(() => {
+        console.log("useEffect berjalan pada count ke - " + count)
+    }, [count]) // perbedaan array kosong dan ada isinya yaitu jika array kosong maka useEffect hanya berjalan sekali, namun jika ada isinya maka useEffect akan berjalan setiap kali isi array berubah
+
+    useEffect(() => {
+        console.log("useEffect name berjalan " + name)
+    }, [name])
 
     return (
         <div className="App">
             <div>
-                <h1>Hello {data}!</h1>
-                <button onClick={() => setData("Rina Pratama")}>Update</button>
+                <h1>useEffect in React</h1>
+                <div>{count}</div>
+                <button onClick={() => setCount(count + 1)}>Update Counter</button>
+
+                <div>
+                    <h3>Hello {name}!!</h3>
+                    <form>
+                        <input type="text" onChange={(e) => setName(e.target.value)} />
+                        <button type="submit">Ubah</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
