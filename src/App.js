116d2708fecbf6331fd9902fc60e53cbc6a43ddb
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2021-10-03 03:39:36 +0700

    belajar neted array dan nested map

--- src/App.js
+++ src/App.js
@@ -5,31 +5,70 @@ function App() {
     const employees = ["Rina", "Pratama", "Prasetyawati", "Irwanto", "Wibowo"]
 
     const students = [
-        {name: "Irwanto", address: "Yogyakarta", contact: 7328292},
-        {name: "Wibowo", address: "Bantul", contact: 7328290    },
-        {name: "Rina", address: "Sleman", contact: 7328291},
-        {name: "Pratama", address: "Gunungkidul", contact: 7328293},
+        {name: "Irwanto", contact: 7328292, address: [
+                {province: "Yogyakarta", distric: "Gunungkidul"},
+                {province: "Bantul", distric: "Pajangan"},
+                {province: "Kulonprogo", distric: "Samigaluh"},
+                {province: "Gunungkidul", distric: "Purwosari"},
+            ]},
+        {name: "Wibowo", contact: 7328290, address: [
+                {province: "Yogyakarta", distric: "Gunungkidul"},
+                {province: "Bantul", distric: "Pajangan"},
+                {province: "Kulonprogo", distric: "Samigaluh"},
+                {province: "Gunungkidul", distric: "Purwosari"},
+            ]},
+        {name: "Rina", contact: 7328291, address: [
+                {province: "Yogyakarta", distric: "Gunungkidul"},
+                {province: "Bantul", distric: "Pajangan"},
+                {province: "Kulonprogo", distric: "Samigaluh"},
+                {province: "Gunungkidul", distric: "Purwosari"},
+            ]},
+        {name: "Pratama", contact: 7328293, address: [
+                {province: "Yogyakarta", distric: "Gunungkidul"},
+                {province: "Bantul", distric: "Pajangan"},
+                {province: "Kulonprogo", distric: "Samigaluh"},
+                {province: "Gunungkidul", distric: "Purwosari"},
+            ]},
     ]
 
     return (
         <div className="App">
-            <h1>List With Bootstrap Tale</h1>
+            <h1>List With Nested Array</h1>
             <Table striped bordered>
                 <thead>
+                    <th>Number</th>
                     <th>Name</th>
-                    <th>Address</th>
                     <th>Contact</th>
+                    <th>Address</th>
                 </thead>
                 <tbody>
-                    {
-                        students.map((item, id) =>
-                            <tr key={id}>
-                                <td>{item.name}</td>
-                                <td>{item.address}</td>
-                                <td>{item.contact}</td>
-                            </tr>
-                        )
-                    }
+                {
+                    students.map((item, i) =>
+                        <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{item.name}</td>
+                            <td>{item.contact}</td>
+                            <td>
+                                <Table>
+                                    <thead>
+                                        <th>Provinsi</th>
+                                        <th>Kabupaten</th>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            item.address.map(data =>
+                                                <tr>
+                                                    <td>{data.province}</td>
+                                                    <td>{data.distric}</td>
+                                                </tr>
+                                            )
+                                        }
+                                    </tbody>
+                                </Table>
+                            </td>
+                        </tr>
+                    )
+                }
                 </tbody>
             </Table>
         </div>
