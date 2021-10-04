3259f310ae866120be34c9eb03959852504b95a8
gerakanamalindonesia <80880673+gerakanamalindonesia@users.noreply.github.com>
2021-10-04 14:56:49 +0700

    mengenal useMemo hook (#20)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import {Fragment} from 'react'
 import './App.css';
-import Child from "./Child";
+import {useState, useMemo} from "react"
+
+/*
+Skenario Awal memahami useMemo
+
+saat button diklik (dalam kasus ini button Update Number) maka state number akan diupdate dan component akan dirender ulang
+namun kenyataanya component item juga ikut terender (alias function multiCount ikut dieksekusi)
+dan kita hanya mau component yang dirender hanyalah komponen yang terpengaruh pada perubahan state (komponen number dan multiCount saja)
+
+kita bisa menggunakan useMemo untuk mengatasi masalah tersebut
+dengan menggunakan useMemo kita bisa skip component yang bersangkutan saat perubahan state terjadi
+*/
 
 function App() {
-    function parentAlert(data) {
-        // data didapat dari variabel user yang dikirim dari props alert(data) di Child.js
-        // Jadi fungsi parentAlert dikirim sebagai props, sehingga component Child.js memiliki fungsi parentAlert yang
-        // mempunyai parameter data, jadi kita tinggal memberi nilai data di Child.js
-        alert(`Hello ${data.name}`)
-    }
+    const [number, setNumber] = useState(0)
+    const [item, setItem] = useState(100)
+
+    // render component multiCountMemo hanya saat state number berubah
+    const multiCountMemo = useMemo(function multiCount() {
+        console.log("Fungsi multiCount berjalan")
+        return number * 5
+    }, [number])
+
     return (
-        <div>
-            <h1>Passing Mempassing State</h1>
-            <Child alert={parentAlert} />
+        <div className="App">
+            <h1>Penggunaan useMemo</h1>
+
+            <h2>Number : {number}</h2>
+            <h2>Item : {item}</h2>
+
+            <h2>{multiCountMemo}</h2>
+            <button onClick={() => setNumber(number + 1)}>Update Number</button>
+            <button onClick={() => setItem(item * 2)}>Update Item</button>
         </div>
     )
 }
